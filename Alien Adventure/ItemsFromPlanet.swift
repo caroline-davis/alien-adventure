c4f4b9d7b1e0a213922c01833d66ae8da49a82fa
Caroline Davis <carol@Jamess-MacBook-Pro.local>
2016-05-18 10:56:29 +1000

    part 2: items from planet

--- Alien Adventure/ItemsFromPlanet.swift
+++ Alien Adventure/ItemsFromPlanet.swift
@@ -9,9 +9,23 @@
 extension Hero {
     
     func itemsFromPlanet(inventory: [UDItem], planet: String) -> [UDItem] {
-        return [UDItem]()
+        var itemsFound = [UDItem]()
+        for item in inventory {
+            if let itemsFromPlanet = item.historicalData["PlanetOfOrigin"]{
+                if planet == itemsFromPlanet as? String {
+                    itemsFound.append(item)
+                }
+            }
+        }        
+        return itemsFound
     }
     
 }
+
+//Find items Origin. Look in UDItem for var historicalData: [String:AnyObject].
+//Use the key “PlanetOfOrigin”, which contains a value that can be converted to a Swift string. 
+//Also try printing out historicalData for an item to see all the key/value pairs it contains.
+
+            // planet parameter has to equal planet of origin value
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 1"
