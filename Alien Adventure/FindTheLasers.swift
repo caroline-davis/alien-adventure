5961b9fe62fc094f15152a10a32685aeff60c952
Caroline Davis <carol@Jamess-MacBook-Pro.local>
2016-06-16 17:46:11 +1000

    part 4: find the lasers

--- Alien Adventure/FindTheLasers.swift
+++ Alien Adventure/FindTheLasers.swift
@@ -6,17 +6,20 @@
 //  Copyright © 2015 Udacity. All rights reserved.
 //
 
+// Checks if any item in containsLaser has the string 'laser' in it
+
 extension Hero {
     
     func findTheLasers() -> (UDItem -> Bool) {
         
         func containsLaser(item: UDItem) -> Bool {
-            return true
+            if ((name?.rangeOfString("laser")) != nil) {
+                return true
+            }
+            return false
         }
-        
         return containsLaser
     }
-    
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 2"
