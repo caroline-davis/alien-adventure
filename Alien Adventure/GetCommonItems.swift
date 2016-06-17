62d1977910bc8b507a50fe293c77108109c82b44
Caroline Davis <carol@Jamess-MacBook-Pro.local>
2016-06-17 16:30:10 +1000

    part 4: get common items

--- Alien Adventure/GetCommonItems.swift
+++ Alien Adventure/GetCommonItems.swift
@@ -8,10 +8,14 @@
 
 extension Hero {
     
-    func getCommonItems(inventory: [UDItem]) -> [UDItem] {
-        return [UDItem]()
-    }
+// This method should take the inventory parameter and remove all of the items that do not have a Common rarity.
     
+    func getCommonItems(inventory: [UDItem]) -> [UDItem] {
+        let onlyCommonList = inventory.filter({(var item:UDItem) -> Bool in
+            return item.rarity.rawValue < 1
+        })
+    return onlyCommonList
+    }
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 6"
--- Alien Adventure/SortLeastToGreatest.swift
+++ Alien Adventure/SortLeastToGreatest.swift
@@ -12,12 +12,11 @@ extension Hero {
 // It then sorts the list in ascending order
     
     func sortLeastToGreatest(inventory: [UDItem]) -> [UDItem] {
-        var orderedList = inventory.sort({
-           $0<$1
+        let orderedList = inventory.sort({
+            $0<$1
         })
         return orderedList
     }
-    
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 5"
