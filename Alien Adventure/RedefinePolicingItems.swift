79f8bbeeaa95c1a4c0b317a7d936f1fce3646c3e
Caroline Davis <carol@Jamess-MacBook-Pro.local>
2016-06-17 13:11:05 +1000

    part 4: redefine policing items

--- Alien Adventure/RedefinePolicingItems.swift
+++ Alien Adventure/RedefinePolicingItems.swift
@@ -20,10 +20,25 @@ extension Hero {
         
         func policingFilter(item: UDItem) throws -> Void {
             
+            // if item.name contains the word Laser throw the NameContainsLaser error
+                let laserName = item.name
+            if laserName.rangeOfString("Laser") != nil {
+                    throw UDPolicingError.NameContainsLaser
+            }
+            // if the item is from planet Cunia throw the ItemFromCunia error
+            if let itemsFromCunia = item.historicalData["PlanetOfOrigin"] {
+                if "Cunia" == itemsFromCunia as? String {
+                    throw UDPolicingError.ItemFromCunia
+                }
+            // if the item has a base value less than 10 throw the ValueLessThan10 error
+                if item.baseValue < 10 {
+                    throw UDPolicingError.ValueLessThan10
+                }
+            }
         }
         
-        return policingFilter        
-    }    
+        return policingFilter
+    }
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 3"
