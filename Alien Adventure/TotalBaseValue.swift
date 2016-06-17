fb1028f83cee3265f9574a6b5b1e6d931aeb78ca
Caroline Davis <carol@Jamess-MacBook-Pro.local>
2016-06-17 16:57:01 +1000

    part 4: total base value

--- Alien Adventure/TotalBaseValue.swift
+++ Alien Adventure/TotalBaseValue.swift
@@ -8,10 +8,14 @@
 
 extension Hero {
     
-    func totalBaseValue(inventory: [UDItem]) -> Int {
-        return 0
-    }
+// Return the total base value of all items in inventory
     
+    func totalBaseValue(inventory: [UDItem]) -> Int {
+        let totalValue = inventory.reduce(0) {
+            $0 + $1.baseValue
+    }
+    return totalValue
+    }
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 7"
